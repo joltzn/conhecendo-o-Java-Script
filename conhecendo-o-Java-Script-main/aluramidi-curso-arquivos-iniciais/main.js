4c9c51fdacbc04d5bc398713ebbc80c5dc2e0e1a
joltzn <134750987+joltzn@users.noreply.github.com>
2023-11-23 22:39:17 -0300

    Add files via upload

---
+++ conhecendo-o-Java-Script-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,11 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+while () {
+    ListaDeTeclas[0].onclick = tocaSomPom;
+}
